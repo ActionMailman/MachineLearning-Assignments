7630769543635a320aa9a4b934c4067f9ece1ebb
Viyan Poonamallee <viyanpoonamallee@gmail.com>
2020-02-20 23:08:03 -0500

    Create Grid.js

---
+++ Currency/Grid.js
@@ -0,0 +1,48 @@
+
+class Grid {
+  constructor(columns, rows, size, inset) {
+    this.cols = columns;
+    this.rows = rows;
+    this.size = size;
+    this.inset = inset;
+    this.gridColor = 255;
+    this.contents = [];
+  }
+
+  draw() {
+    for (let row = 0; row < this.rows; row++)
+      for (let col = 0; col < this.cols; col++) this.drawSquare(col,row,this.gridColor);
+  }
+  
+  drawSquare(c,r,color) {
+    fill(color);
+    rect(this.inset + c * this.size, this.inset + r * this.size, this.size, this.size);
+  }
+  
+   addStuff(thing) {
+    this.contents.push(thing);
+  }
+  
+  stuffAt(x,y) {
+    /*
+    pass in the coordinates of the agent and
+    search through the contents list to see 
+    if there is something added to the grid with the same coordinates. 
+    return true or false.
+    */
+  }
+    
+
+  drawAgent(agent) {
+    // Change this print to a text() and display the agent position at the top of the canvas
+    print(agent.col + " " + agent.row);
+    this.drawSquare(agent.col,agent.row,color(255,0,0));
+  }
+
+  
+  drawCurrency(currency) {
+    // Change this print to a text() and display the agent position at the top of the canvas
+    print(currency.col + " " + currency.row);
+    this.drawSquare(currency.col,currency.row,color(0,255,0));
+  }
+}
